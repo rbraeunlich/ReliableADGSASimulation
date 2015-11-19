7d9a76f84584e5c3083d30c9a038bdfec4e11773
Job Wook J.R.C <jrcright1004@gmail.com>
2015-11-19 19:11:53 +0900

    modify

--- src/main/java/kr/ac/kaist/ds/groupd/topology/InterestInitializer.java
+++ src/main/java/kr/ac/kaist/ds/groupd/topology/InterestInitializer.java
@@ -9,10 +9,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-<<<<<<< HEAD
 import kr.ac.kaist.ds.groupd.information.ManagerGroups;
-=======
->>>>>>> 938bd0ce7dc7193a4ca301d51340b209d52b4448
 import kr.ac.kaist.ds.groupd.parse.Genre;
 import kr.ac.kaist.ds.groupd.parse.Movie;
 import kr.ac.kaist.ds.groupd.parse.MovieParser;
@@ -30,7 +27,6 @@ import peersim.core.Network;
  */
 public class InterestInitializer implements Control {
 
-<<<<<<< HEAD
     private static final String PAR_PROT = "protocol";
 
     private static ManagerGroups MG = new ManagerGroups(Network.size(), ManagerGroups._BLANCE_MODE);
@@ -110,74 +106,5 @@ public class InterestInitializer implements Control {
     static ManagerGroups getManagerGroups() {
         return InterestInitializer.MG;
     }
-=======
-	private static final String PAR_PROT = "protocol";
-
-	private final int pid;
-
-	public InterestInitializer(String prefix) {
-		this.pid = Configuration.getPid(prefix + "." + PAR_PROT);
-	}
-
-	@Override
-	public boolean execute() {
-		Collection<Movie> movies = parseMovies();
-		Collection<Rating> ratings = parseRatings(movies);
-		Map<Long, List<Rating>> ratingsByUserId = ratings.stream().collect(Collectors.groupingBy(Rating::getUserId));
-		for (int i = 0; i < Network.size(); i++) {
-			List<Rating> filteredRatings = ratingsByUserId.get(Long.valueOf(i + 1));
-			double[] interestVector = createInterestVector(filteredRatings);
-			InterestProtocol protocol = (InterestProtocol) Network.get(i)
-					.getProtocol(pid);
-			protocol.setInterestVector(interestVector);
-		}
-		return false;
-	}
-
-	private Collection<Rating> parseRatings(Collection<Movie> movies) {
-		URL ratingsFile = this.getClass().getClassLoader()
-				.getResource("kr/ac/kaist/ds/groupd/topology/ratings.dat");
-		Collection<Rating> ratings = new RatingParser(movies, ratingsFile)
-				.parseRatings();
-		return ratings;
-	}
-
-	private Collection<Movie> parseMovies() {
-		URL moviesFile = this.getClass().getClassLoader()
-				.getResource("kr/ac/kaist/ds/groupd/topology/movies.dat");
-		Collection<Movie> movies = new MovieParser(moviesFile).parseMovies();
-		return movies;
-	}
-
-	/**
-	 * Creates a vector based on the ratings in the Movielens dataset.
-	 * For every gerne the user's average rating is calculated and placed in the vector.
-	 * The position inside the vector is determined by the ordinal inside {@link Genre}.
-	 * @param filteredRatings
-	 * @return
-	 */
-	protected double[] createInterestVector(
-			List<Rating> filteredRatings) {
-		List<Genre> genreList = Arrays.asList(Genre.values());
-		double[] ratings = new double[genreList.size()];
-		double[] madeRatingsPerGenre = new double[genreList.size()];
-		// FIXME I am unsure if averaging is the best way
-		for (Rating rating : filteredRatings) {
-			Set<Genre> genres = rating.getMovie().getGenres();
-			for (Genre genre : genres) {
-				ratings[genre.ordinal()] = ratings[genre.ordinal()] + rating.getRating();
-				madeRatingsPerGenre[genre.ordinal()] = madeRatingsPerGenre[genre.ordinal()] + 1.0;
-			}
-		}
-		double[] interestVector = new double[genreList.size()];
-		for(int i = 0; i < interestVector.length; i++){
-			if(madeRatingsPerGenre[i] == 0){
-				continue;
-			}
-			interestVector[i] = ratings[i] / madeRatingsPerGenre[i];
-		}
-		return interestVector;
-	}
->>>>>>> 938bd0ce7dc7193a4ca301d51340b209d52b4448
 
 }
