5263beeee75e2331a40503e87783f73f3fa48946
Job Wook J.R.C <jrcright1004@gmail.com>
2015-11-22 23:00:52 +0900

    implement something that ADGSA's 1~4 line parts ( will change the token  to another) such as check the final node, and send message neighbors

--- src/main/java/kr/ac/kaist/ds/groupd/search/SearchQuery.java
+++ src/main/java/kr/ac/kaist/ds/groupd/search/SearchQuery.java
@@ -7,14 +7,19 @@ import peersim.core.Node;
 
 public class SearchQuery {
 
+    // start node
 	private int source;
 
+	// destination node
 	private int destination;
 
+	// backstep
 	private boolean backward;
 
+	// visited nodes
 	private List<Node> visitedNodes;
 
+	// visited groups
 	private List<GroupName<?>> visitedGroups;
 
 }
--- src/main/java/kr/ac/kaist/ds/groupd/search/impl/SearchProtocolImpl.java
+++ src/main/java/kr/ac/kaist/ds/groupd/search/impl/SearchProtocolImpl.java
@@ -1,58 +1,99 @@
+
 package kr.ac.kaist.ds.groupd.search.impl;
+
+import java.util.ArrayList;
 
 import kr.ac.kaist.ds.groupd.search.SearchProtocol;
 import kr.ac.kaist.ds.groupd.search.SearchQuery;
+import peersim.config.FastConfig;
+import peersim.core.CommonState;
+import peersim.core.Linkable;
 import peersim.core.Node;
 import kr.ac.kaist.ds.groupd.interest.InterestProtocol;
+import kr.ac.kaist.ds.groupd.example.token.TokenProtocol;
 import kr.ac.kaist.ds.groupd.groupname.GroupNameProtocol;
 
 public class SearchProtocolImpl implements SearchProtocol {
 
-	private static final String PAR_NAME_PROTOCOL = "naming";
+    private static final String PAR_NAME_PROTOCOL = "naming";
 
-	private static final String PAR_INTEREST_GROUP_PROTOCOL = "interestgroup";
+    private static final String PAR_INTEREST_GROUP_PROTOCOL = "interestgroup";
 
-	private InterestProtocol interestProtocol;
+    private InterestProtocol interestProtocol;
 
-	private GroupNameProtocol groupNameProtocol;
+    private GroupNameProtocol groupNameProtocol;
 
-	private SearchQuery searchQuery;
+    private SearchQuery searchQuery;
+    
+    static ArrayList<Long> checkForPeers;
 
-	/**
-	 * Performs the forward search based on the ADGSA algorithm.
-	 */
-	private void performSearch() {
+    private boolean token;
 
-	}
-	/**
-	 * Performs the backtracking, based on our group naming scheme.
-	 */
-	private void performBacktracking() {
+    public SearchProtocolImpl(String name) {
+    }
 
-	}
+    public boolean hasToken() {
+        return token;
+    }
 
+    public void resetToken() {
+        this.token = false;
+    }
 
-	/**
-	 * @see kr.ac.kaist.ds.groupd.search.SearchProtocol#setSearchQuery(kr.ac.kaist.ds.groupd.search.SearchQuery)
-	 */
-	public void setSearchQuery(SearchQuery q) {
+    /**
+     * Performs the forward search based on the ADGSA algorithm.
+     */
+    private void performSearch(Node node, int pid) {
 
-	}
-	
-	@Override
-	public Object clone() {
-		try {
-			return super.clone();
-		} catch (CloneNotSupportedException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+        //1~2
+        if(checkForPeers.contains(node.getID()))
+            return;
+        else
+            checkForPeers.add(node.getID());
+        
+        //3~4 token will be change other.
+        if (true == token) {
 
-	@Override
-	public void nextCycle(Node node, int protocolID) {
-		// TODO Auto-generated method stub
-		
-	}
+            Linkable linkable = (Linkable)node.getProtocol(FastConfig.getLinkable(pid));
+
+            for (int i = 0; i < linkable.degree(); i++) {
+                Node neighbor = linkable.getNeighbor(CommonState.r.nextInt(i));
+                TokenProtocol protocol = (TokenProtocol)neighbor.getProtocol(pid);
+                protocol.setToken(true);
+            }
+        }
+
+    }
+
+    /**
+     * Performs the backtracking, based on our group naming scheme. backTracking
+     * follow previous steps
+     */
+    private void performBacktracking() {
+
+    }
+
+    /**
+     * @see kr.ac.kaist.ds.groupd.search.SearchProtocol#setSearchQuery(kr.ac.kaist.ds.groupd.search.SearchQuery)
+     */
+    public void setSearchQuery(SearchQuery q) {
+
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void nextCycle(Node node, int protocolID) {
+        performSearch(node, protocolID);
+
+    }
 
 }
