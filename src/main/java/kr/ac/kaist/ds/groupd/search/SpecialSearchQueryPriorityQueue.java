7a83bf0110fb0b98bf1a069613e76744e4c6bb77
Ronny Bräunlich <r.braeunlich@gmail.com>
2015-11-28 23:08:15 +0900

    unneccessary queries are only being deleted for forward queries

--- src/main/java/kr/ac/kaist/ds/groupd/search/SpecialSearchQueryPriorityQueue.java
+++ src/main/java/kr/ac/kaist/ds/groupd/search/SpecialSearchQueryPriorityQueue.java
@@ -1,45 +1,51 @@
-
 package kr.ac.kaist.ds.groupd.search;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 
-public class SpecialSearchQueryPriorityQueue extends SearchQueryPriorityQueue<SearchQuery> {
+public class SpecialSearchQueryPriorityQueue extends
+		SearchQueryPriorityQueue<SearchQuery> {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private final int fixedSizeOfQueue;
+	private final int fixedSizeOfQueue;
 
-    public SpecialSearchQueryPriorityQueue(int initialCapacity,
-            Comparator<? super SearchQuery> comparator) {
-        super(initialCapacity, comparator);
-        this.fixedSizeOfQueue = initialCapacity;
-    }
+	public SpecialSearchQueryPriorityQueue(int initialCapacity,
+			Comparator<? super SearchQuery> comparator) {
+		super(initialCapacity, comparator);
+		this.fixedSizeOfQueue = initialCapacity;
+	}
 
-    @Override
-    public boolean offer(SearchQuery searchQuery) {
+	@Override
+	public boolean offer(SearchQuery searchQuery) {
 
-        deleteUnnecessarySearchQuery(searchQuery);
+		if (this.size() >= fixedSizeOfQueue) {
+			SearchQuery element = remove();
+			if (this.comparator().compare(element, searchQuery) == -1) {
+				if (searchQuery.isBackward()) {
+					deleteUnnecessarySearchQuery(searchQuery);
+				}
+				return super.offer(searchQuery);
+			}
+			return false;
+		}
 
-        if (this.size() >= fixedSizeOfQueue) {
-            SearchQuery element = remove();
-            if (this.comparator().compare(element, searchQuery) == -1)
-                return super.offer(searchQuery);
+		return super.offer(searchQuery);
+	}
 
-            return false;
-        }
+	/**
+	 * Delete the {@link SearchQuery}s that are still forward searching because
+	 * this query already found the destination.
+	 */
+	private void deleteUnnecessarySearchQuery(SearchQuery searchQuery) {
+		for (SearchQuery q : new ArrayList<SearchQuery>(this)) {
+			if (searchQuery.getDestination() == q.getDestination()
+					&& q.isBackward() == false
+					&& searchQuery.getSource() == q.getSource()) {
+				this.remove(q);
+			}
+		}
 
-        return super.offer(searchQuery);
-    }
-
-    private void deleteUnnecessarySearchQuery(SearchQuery searchQuery) {
-        for( SearchQuery q : new ArrayList<SearchQuery>(this)) {
-            if (searchQuery.getDestination() == q.getDestination() && q.isBackward() == false
-                    && searchQuery.getSource() == q.getSource()) {
-                this.remove(q);
-            }
-        }
-
-    }
+	}
 
 }
